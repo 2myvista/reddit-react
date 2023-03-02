fa71c2f334b80759d7bfba115983a7fe02387781
myvista <myvista@ya.ru>
2023-03-02 13:38:47 +0300

    unit4

--- M2/cfg/webpack.client.config.js
+++ M2/cfg/webpack.client.config.js
@@ -5,6 +5,8 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
 const IS_PROD = NODE_ENV === 'production';
+const GLOBAL_CSS_REGEXP = /\.global\.css$/;
+
 
 function setupDevtool() {
     if (IS_DEV) return 'eval';
@@ -13,7 +15,7 @@ function setupDevtool() {
 
 module.exports = {
     resolve: {
-        extensions: ['.js', '.jsx', '.json'], 
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'], 
         alias: {
             'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
         }
@@ -35,7 +37,7 @@ module.exports = {
                 use: ['ts-loader']
             },
             {
-                test: /\.less$/,
+                test: /\.css$/,
                 use: [
                     'style-loader',
                     {
@@ -49,6 +51,12 @@ module.exports = {
                     },
                     'less-loader', 
                 ],
+                exclude: GLOBAL_CSS_REGEXP,
+            },
+            {
+                test: GLOBAL_CSS_REGEXP,
+                use: ['style-loader', 'css-loader']
+
             }
         ]
     },
