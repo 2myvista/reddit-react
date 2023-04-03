1b7b593d801adecb598ad2199bd1b2433e390197
myvista <myvista@ya.ru>
2023-04-03 17:38:01 +0300

    testing functions

--- M2/src/shared/Dropdown/__tests__/Dropdown.test.tsx
+++ M2/src/shared/Dropdown/__tests__/Dropdown.test.tsx
@@ -6,8 +6,15 @@ describe('Drodown', ()=> {
 	test('should render', () => {
 		const wrapper = shallow(<Dropdown children={<div/>} button={<button/>}/>)
 		expect(wrapper).toBeDefined();
-		expect(wrapper.find('#button')).toBeDefined();
+		console.log(wrapper.find('div.container').debug());
+		
+		expect(wrapper.find('div.container').isEmptyRender()).toBeFalsy();
 	  
 	})
 	
+	test('should render (snapshort)', ()=> {
+		const wrapper = shallow(<Dropdown children={<div/>} button={<button/>}/>)
+
+		expect(wrapper).toMatchSnapshot()
+	})
 })
