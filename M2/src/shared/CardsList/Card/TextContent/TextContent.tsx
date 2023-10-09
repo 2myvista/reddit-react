6abcb05590e32d16a21b764953783cad29f13555
myvista <myvista@ya.ru>
2023-10-09 21:11:29 +0300

    unit14 linkId

--- M2/src/shared/CardsList/Card/TextContent/TextContent.tsx
+++ M2/src/shared/CardsList/Card/TextContent/TextContent.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from './textcontent.css';
 import { Post } from '../../../Post';
+import {Link} from 'react-router-dom';
 
 interface ICardProps {
 	id: string;
@@ -13,6 +14,28 @@ interface ICardProps {
 	selftext: string;
 }
 
+export function TextContent({id, subreddit, userNameProp, title, published=10, avatar, permalink, selftext }:ICardProps) {
+	avatar = avatar? avatar: 'https://cdn.dribbble.com/users/877810/avatars/normal/bca2fa37b3bdbe545749e7903fb89dcd.png';
+	
+  return (
+		<div className={styles.textContent} >
+			<div className={styles.metaData} >
+				<div className={styles.userLink} >
+					<img className={styles.avatar} src={avatar} alt="avatar" />
+					<a href="#user-url" className={styles.username}>{userNameProp}</a>
+				</div>
+				<span className={styles.createdAt}>
+					<span className={styles.publishedLabel}>опубликовано </span>
+					{published}</span>
+			</div>
+			<h2 className={styles.title}>
+				<Link to={`posts/${id}`}  className={styles.postLink}>{title}</Link>	
+			</h2>
+			
+		</div>
+  );
+}
+/* 09.10.23 before change
 export function TextContent({id, subreddit, userNameProp, title, published=10, avatar, permalink, selftext }:ICardProps) {
 	const [isModalOpened, setIsModalOpened]= useState(false);
 	avatar = avatar? avatar: 'https://cdn.dribbble.com/users/877810/avatars/normal/bca2fa37b3bdbe545749e7903fb89dcd.png';
@@ -38,3 +61,4 @@ export function TextContent({id, subreddit, userNameProp, title, published=10, a
 		</div>
   );
 }
+ */
