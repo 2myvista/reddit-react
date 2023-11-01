45e65e7a0423c369442284af01adaef540c99068
myvista <myvista@ya.ru>
2023-11-01 18:42:45 +0300

    unit16

--- M2/src/shared/CommentForm/CommentForm.tsx
+++ M2/src/shared/CommentForm/CommentForm.tsx
@@ -5,12 +5,26 @@ import { Console } from 'console';
 import { useForm } from 'react-hook-form';
 import { useSelector,useDispatch } from 'react-redux';
 import { RootState, updateComment } from '../../redux/store/reducer';
+import { makeAutoObservable } from "mobx";
+import { observer } from "mobx-react-lite";
 
  type Props = {
 	comment: string;
 }
 
-export function CommentForm() {
+class CommentMobx {
+	value ='text from Mobx';
+	constructor() {
+			makeAutoObservable(this);
+	}
+	updateValue(newValue: string) {
+		this.value = newValue;
+	}
+}
+
+const commentMobx = new CommentMobx();
+
+export const  CommentForm = observer(()=> {
 	const {
 		register,
 		setValue,
@@ -25,35 +39,37 @@ export function CommentForm() {
 
 	const onSubmit = handleSubmit((data) => {
 			console.log(data);
-			//alert(data.comment);
 			console.log(errors);
 			
-			dispatch(updateComment(data.comment));
+			commentMobx.updateValue(data.comment)
+			//dispatch(updateComment(data.comment));
 	})
 
 
  	function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
 		console.log(event.target.value);
-		dispatch(updateComment(event.target.value))
+		//dispatch(updateComment(event.target.value))
+		commentMobx.updateValue(event.target.value)
 	}
  	// получить стейт 
 	// const value = useSelector<RootState, string>(state=>state.commentText);
 	//console.log(value);
 
-	const dispatch = useDispatch();
+	//const dispatch = useDispatch();
 	
 	return (
 	<form className={styles.form} onSubmit={onSubmit}>
-		<textarea className={styles.input} {...register("comment", {required: true,  minLength: 6})}
+		<textarea className={styles.input} {...register("comment", {required: true,  minLength: 20})}
 		aria-invalid={errors.comment ? "true" : undefined} 
-		placeholder='заполните не менее 6 символов'
+		placeholder='заполните не менее 20 символов'
 		onChange={handleChange}
+		value={commentMobx.value}
 		/>
 		
-		{errors.comment?.type === "minLength" && (<div>минимум 6 символов</div>)}
+		{errors.comment?.type === "minLength" && (<div>минимум 20 символов</div>)}
 		{errors.comment?.type === "required" && (<div>необходимо заполнить это поле</div>)}
 		
 		<button type="submit" className={styles.button}>Комментировать</button>
 	</form>	
   );
-}
+})
